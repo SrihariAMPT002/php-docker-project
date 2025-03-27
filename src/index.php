ce96091f95d36bb7bd05406ffd2958187c29b393
SrihariAMPT002 <srihari.tagili@amperadei.com>
2025-03-27 17:38:50 +0530

    SQL Connection

--- src/index.php
+++ src/index.php
@@ -20,4 +20,19 @@ echo "<html>
     <p>RTS demo trails into docker</p>
 </body>
 </html>";
+
+$servername = getenv('DB_HOST');  
+$username = getenv('DB_USER');    
+$password = getenv('DB_PASSWORD'); 
+$dbname = "test"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully to the 'test' database"; 
+
+
 ?>
