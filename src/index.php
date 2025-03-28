80dcfc1c5a9ce9bfe0a63477a1378b2c58bb3188
SrihariAMPT002 <srihari.tagili@amperadei.com>
2025-03-28 14:16:54 +0530

    Connection corrections in index page

--- src/index.php
+++ src/index.php
@@ -1,35 +1,29 @@
 <?php
 echo "<html>
 <head>
-    <title>Welcome Page</title>
+    <title>Welcome</title>
     <style>
-        body {
-            background-color:rgb(255, 240, 242); /* Light blue background */
-            color: #333; /* Dark text color */
-            font-family: Arial, sans-serif;
-            text-align: center;
-            padding: 50px;
-        }
         h1 {
             color: #4CAF50; /* Green color for the header */
         }
     </style>
 </head>
 <body>
-    <h1>Welcome to My Website!</h1>
-    <p>RTS demo trails into docker</p>
+    <h1>SQL Connection!</h1>
+    <p>RTS demo trails into docker sql</p>
 </body>
 </html>";
-$servername = "mysql"; 
-$username = "root";
-$password = "root";
-$dbname = "test"; 
+
+$servername = "mysql-container"; 
+$username = "testing";
+$password = "rguktbasar";
+$dbname = "testing"; 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
-echo "Connected successfully to the 'test' database";
+echo "Connected successfully to the 'testing' database";
 ?>
